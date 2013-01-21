da7a8b9d15afbd0ee473811d002c9eabd0fcfd8c
Tory Jarmain <tjarmain@gmail.com>
2013-01-21 16:09:24 -0500

    Create a hash to store individual properties of each apartment

--- scraper.rb
+++ scraper.rb
@@ -5,12 +5,20 @@ craigslist_apartments_url = "http://toronto.en.craigslist.ca/apa/"
 
 craigslist_apartments = Nokogiri::HTML(open(craigslist_apartments_url))
 
-# Create an array called page_contents
 page_contents = []
 
 craigslist_apartments.css('p.row').each do |item|
-  # Use << (sometimes called a shovel) to add each of the links to the
-  # page_contents array. When you run this script, nothing will be printed
-  # to the terminal but page_contents will contain all of the items
+  # We're going to store data using an array of hashes
+  # We'll need a hash that we can use to store the following data
+  # - description
+  # - price
+  # - number of bedrooms
+  # - square footage
+  # Start by creating a hash called 'apartment'
+  # We'll soon be able to access individual items in the page_contents array
+  # by using commands like page_contents[0]["description"]
+  apartment = Hash.new
+
   page_contents << item.css('a')
+
 end
