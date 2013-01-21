b4ed5cd250ffaefb60f8934aa0e52ec2cf8c3b98
Tory Jarmain <tjarmain@gmail.com>
2013-01-21 17:36:43 -0500

    Create a method to parse the number of bedrooms

--- scraper.rb
+++ scraper.rb
@@ -7,29 +7,26 @@ craigslist_apartments = Nokogiri::HTML(open(craigslist_apartments_url))
 
 page_contents = []
 
-def parse_description(description)
-  # Here's a sample of a line we need to parse
-  # $1750 / 1br - 650ft² -
-  # We can use a regular expression to find the price given the line above
-  # One thing to note: when we run the match method on the description it will return
-  # a MatchData object, like so:
-  #  <MatchData "$1750" 1:"1750">
-  # We'll need to access the "1750" the same way we access arrays
-  # If you're totally confused it's worth referencing a guide on regular expressions
-  # http://net.tutsplus.com/tutorials/ruby/ruby-for-newbies-regular-expressions/
-
-  # The following block of code checks to see if the string 'description'
-  # contains a price
+def parse_price(description)
   if description.match(/\$(\d+)/)
-    # if the description contains a price, assign it to the variable price
     price = description.match(/\$(\d+)/)
-    # because we're using the match method we're dealing with a MatchData object
-    # we can pull the price out of it by calling price[1]
     return price[1]
   else
-    # if the regular expression can't find a price it will return 'nil'
-    # this can cause the script to break, so we're setting a price of $0
-    # for apartment listings that don't contain one
+    return 0
+  end
+end
+
+# Create a method to parse the number of bedrooms
+def parse_number_of_bedrooms(description)
+  # Craigslist uses a format for bedrooms that looks like this: 2br
+  # The regular expression below will match any digits followed by "br"
+  if description.match(/\d+br/)
+    number_of_bedrooms = description.match(/\d+br/)
+    # This will assign a string (e.g., "2br") to number_of_bedrooms
+    # We need to strip out any non-digit characters ("br"), which we can do
+    # using a second regular expression
+    return number_of_bedrooms[0].match(/\d/).to_s
+  else
     return 0
   end
 end
@@ -38,15 +35,13 @@ craigslist_apartments.css('p.row').each do |item|
   apartment = Hash.new
 
   apartment["description"] = item.css('a').text
+  apartment["price"] = parse_price(item.css('.itemph').text)
 
-  # Now we can use our parse_description method to take the contents
-  # of the .itemph element and parse them
-  apartment["price"] = parse_description(item.css('.itemph').text)
+  # Let's now add the number of bedrooms to our hash
+  apartment["number_of_bedrooms"] = parse_number_of_bedrooms(item.css('.itemph').text)
 
   page_contents << apartment
 
 end
 
-# When we print out the contents of our array we should see a
-# description and price listed for each item in the array
 puts page_contents
